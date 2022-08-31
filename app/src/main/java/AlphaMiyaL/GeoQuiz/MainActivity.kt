4eb9bb9b55c513b080426d1c7c7c5255e7a2acf2
Brian Song <79394580+AlphaMiyaL@users.noreply.github.com>
2022-08-31 16:37:06 -0700

    Moved Toast to top

--- app/src/main/java/AlphaMiyaL/GeoQuiz/MainActivity.kt
+++ app/src/main/java/AlphaMiyaL/GeoQuiz/MainActivity.kt
@@ -2,9 +2,13 @@ package AlphaMiyaL.GeoQuiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+
+//Ch1 done
+//Challenge 1 done
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
@@ -17,20 +21,38 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
 
-        trueButton.setOnClickListener{view: View ->
-            Toast.makeText(
-                this,
-                R.string.correct_toast,
-                Toast.LENGTH_SHORT)
-                .show()
+        //Have to make separate functions due to setGravity and show both returning void
+        fun trueButtonPress(trueBut: Toast=Toast.makeText(
+            this,
+            R.string.correct_toast,
+            Toast.LENGTH_SHORT)){
+            trueBut.setGravity(Gravity.TOP, 0, 0)
+            trueBut.show()
         }
 
+        fun falseButtonPress(falseBut: Toast=Toast.makeText(
+        this,
+        R.string.incorrect_toast,
+        Toast.LENGTH_SHORT)){
+            falseBut.setGravity(Gravity.TOP, 0, 0)
+            falseBut.show()
+        }
+
+        trueButton.setOnClickListener{view ->
+//            Toast.makeText(
+//                this,
+//                R.string.correct_toast,
+//                Toast.LENGTH_SHORT)
+//                .setGravity(Gravity.TOP, 0, 0)
+            trueButtonPress()
+        }
         falseButton.setOnClickListener{view: View ->
-            Toast.makeText(
-                this,
-                R.string.incorrect_toast,
-                Toast.LENGTH_SHORT)
-                .show()
+//            Toast.makeText(
+//                this,
+//                R.string.incorrect_toast,
+//                Toast.LENGTH_SHORT)
+//                .setGravity(Gravity.TOP, 0, 0)
+            falseButtonPress()
         }
     }
 }
