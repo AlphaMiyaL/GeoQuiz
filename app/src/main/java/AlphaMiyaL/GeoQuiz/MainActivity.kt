ff0575023138c83049133d24f65316d39cb96dda
Brian Song <79394580+AlphaMiyaL@users.noreply.github.com>
2022-09-07 21:31:14 -0700

    quiz graded, no repeat answers, arrows changed

--- app/src/main/java/AlphaMiyaL/GeoQuiz/MainActivity.kt
+++ app/src/main/java/AlphaMiyaL/GeoQuiz/MainActivity.kt
@@ -2,23 +2,31 @@ package AlphaMiyaL.GeoQuiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
-import org.w3c.dom.Text
+import kotlin.math.round
 
 //Main Activity
 
-//Ch1 done
-//Challenge 1 done
+//Ch1-3 done
+//Prev Button Challenge done
+//Button to ImageButton Challenge done
+//Preventing Repeat Answers Challenge done
+//Graded Quiz Challenge done
+
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
-    private lateinit var prevButton: Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var prevButton: ImageButton
     private lateinit var questionTextView: TextView
 
     private var currentIndex = 0
@@ -30,8 +38,13 @@ class MainActivity : AppCompatActivity() {
         Question(R.string.question_raids, false),
         Question(R.string.question_pvp, true))
 
+    private var questionsAnswered = 0
+    private var questionAnsweredList = IntArray(questionList.size)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //Log.d(TAG, "OnCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.true_button)
@@ -49,6 +62,7 @@ class MainActivity : AppCompatActivity() {
         nextButton.setOnClickListener{view: View ->
             currentIndex=(currentIndex+1)%questionList.size
             updateQuestion()
+            buttonEnablerDisabler()
         }
         prevButton.setOnClickListener { view: View ->
             currentIndex=(currentIndex-1)
@@ -56,28 +70,104 @@ class MainActivity : AppCompatActivity() {
                 currentIndex=questionList.size-1
             }
             updateQuestion()
+            buttonEnablerDisabler()
         }
 
         updateQuestion()
     }
+
+//    override fun onStart() {
+//        super.onStart()
+//        Log.d(TAG, "onStart() called")
+//    }
+//    override fun onResume() {
+//        super.onResume()
+//        Log.d(TAG, "onResume() called")
+//    }
+//    override fun onPause() {
+//        super.onPause()
+//        Log.d(TAG, "onPause() called")
+//    }
+//    override fun onStop() {
+//        super.onStop()
+//        Log.d(TAG, "onStop() called")
+//    }
+//    override fun onDestroy() {
+//        super.onDestroy()
+//        Log.d(TAG, "onDestroy() called")
+//    }
 
     private fun updateQuestion(){
         val questionTextResId = questionList[currentIndex].textResId
         questionTextView.setText(questionTextResId)
     }
 
+    private fun buttonEnablerDisabler(){
+        //re-enable true false buttons if not answered question
+        if(questionAnsweredList[currentIndex]==0){
+            enableButton(trueButton)
+            enableButton(falseButton)
+        }
+        else{
+            disableButton(trueButton)
+            disableButton(falseButton)
+        }
+    }
+
+    private fun quizFinished(){
+        //check if all questions were answered
+        if(questionsAnswered == questionList.size) {
+            var questions_correct = 0
+            for (i in 0 until questionsAnswered) {
+                if (questionAnsweredList[i] == 1) {
+                    questions_correct++
+                }
+            }
+            //displays toast of percentage done
+            val messageResId = getString(R.string.questions_correct) + round((questions_correct.toDouble() / questionList.size) * 100) + getString(R.string.percent)
+            val toasty: Toast = Toast.makeText(this,messageResId,Toast.LENGTH_SHORT)
+            toasty.setGravity(Gravity.TOP, 0, 0)
+            toasty.show()
+        }
+    }
+
     private fun checkAnswer(userAnswer: Boolean){
         val correctAnswer = questionList[currentIndex].answer
-        val messageResId = if (userAnswer == correctAnswer){
+        val correct = userAnswer == correctAnswer
+        val messageResId = if (correct){
             R.string.correct_toast
         }
         else{
             R.string.incorrect_toast
         }
 
+        //disable button after one answer for no repeats
+        disableButton(trueButton)
+        disableButton(falseButton)
+        questionAnsweredList[currentIndex] = if(correct){
+            1
+        }
+        else{
+            2
+        }
+        questionsAnswered++
+        quizFinished()
+
         //Set gravity and show will both return null which is why it made into a val
-        val Toasty: Toast = Toast.makeText(this,messageResId,Toast.LENGTH_SHORT)
-        Toasty.setGravity(Gravity.TOP, 0, 0)
-        Toasty.show()
+//        val Toasty: Toast = Toast.makeText(this,messageResId,Toast.LENGTH_SHORT)
+//        Toasty.setGravity(Gravity.TOP, 0, 0)
+//        Toasty.show()
     }
+
+    private fun disableButton(button: Button){
+         button.isEnabled = false
+         button.isClickable = false
+    }
+
+    private fun enableButton(button: Button){
+        button.isEnabled = true
+        button.isClickable = true
+    }
+
+
 }
